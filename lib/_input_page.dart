15b910dc8dace9a64d38c9123169d00fef7f36b1
TakshalModi02 <202151170@iiitvadodara.ac.in>
2022-10-23 16:29:45 +0530

    Second One

--- lib/_input_page.dart
+++ lib/_input_page.dart
@@ -15,13 +15,128 @@ class _InputPageState extends State<InputPage> {
         centerTitle: true,
         title: const Text("BMI Calculator"),
       ),
-      body: const Center(
-        child: Text("This is body!!"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
+
+      body: SafeArea(
+        child: Column(
+          children: [
+
+            const Expanded(flex: 1, child: SizedBox()),
+
+            Expanded(
+              flex: 10,
+              child: Row(
+                children: const [
+                  Expanded(
+                      flex: 1,
+                      child: SizedBox()
+                  ),
+                  Expanded(
+                    flex: 10,
+                    child: Card(),
+                  ),
+                  Expanded(
+                      flex: 1,
+                      child: SizedBox()
+                  ),
+                  Expanded(
+                    flex: 10,
+                    child: Card(),
+                  ),
+                  Expanded(
+                      flex: 1,
+                      child: SizedBox()
+                  ),
+                ],
+              ),
+            ),
+
+            const Expanded(flex: 1, child: SizedBox()),
+
+            Expanded(
+              flex: 10,
+              child: Row(
+                children: const [
+                  Expanded(
+                      flex: 1,
+                      child: SizedBox()
+                  ),
+                  Expanded(
+                    flex: 20,
+                    child: Card(),
+                  ),
+                  Expanded(
+                      flex: 1,
+                      child: SizedBox()
+                  ),
+                ],
+              ),
+            ),
+
+            const Expanded(flex: 1, child: SizedBox()),
+
+            Expanded(
+              flex: 10,
+              child: Row(
+                children: const [
+                  Expanded(
+                      flex: 1,
+                      child: SizedBox()
+                  ),
+                  Expanded(
+                    flex: 10,
+                    child: Card(c: Color(0xFF1D1F33),)
+                  ),
+                  Expanded(
+                      flex: 1,
+                      child: SizedBox()
+                  ),
+                  Expanded(
+                    flex: 10,
+                    child: Card(),
+                  ),
+                  Expanded(
+                      flex: 1,
+                      child: SizedBox()
+                  ),
+                ],
+              ),
+            ),
+            const Expanded(flex: 1, child: SizedBox()),
+            
+            Expanded(
+              flex: 5,
+              child: Container(
+                color: const Color(0xFFEB1555),
+                child: const Center(
+                  child: Text(
+                    "CALCULATE YOUR BMI",
+                    style: TextStyle(fontSize: 20,),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
 }
+
+class Card extends StatelessWidget {
+
+  final Color c;
+
+  const Card({super.key, this.c = const Color(0xFF1D1F33),});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: c,
+      ),
+    );
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ void main() {
   runApp(const BMI_Calculator());
 }
 
+MaterialColor colorCustom = const MaterialColor(0xFF0A0D22, <int, Color>{0: Color(0xFF0A0D22)});
+
 class BMI_Calculator extends StatelessWidget {
   const BMI_Calculator({Key? key}) : super(key: key);
 
@@ -12,7 +14,19 @@ class BMI_Calculator extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      // theme: ThemeData.dark().copyWith(primaryColor: Colors.red),
       theme: ThemeData(
+
+        appBarTheme: const AppBarTheme(color: Color(0xFF0A0D22), elevation: 5),
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(backgroundColor: Color(0xFFEB1555)),
+        scaffoldBackgroundColor: const Color(0xFF0A0D22),
+        textTheme: const TextTheme(bodyText1: TextStyle(color: Colors.white), bodyText2: TextStyle(color: Colors.white)),
+
+
+
+
+        // primarySwatch: colorCustom,
+
 
         //Both this two is not working.. I want to make theme having color of App bar and body with Hexcode #0A0D22
 
